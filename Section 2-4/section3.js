6c1c30d550eb986045c62169f554ba60299f4b77
Ghulsan Savana Ahasya <ghulsan.savanaa@gmail.com>
2023-04-06 15:50:59 +0700

    membuat 3 contoh dari Get child elements

--- Section 2-4/section3.js
+++ Section 2-4/section3.js
@@ -1,3 +1,12 @@
+// get first child element
+let childPertama = document.getElementById("menu")
+console.log(childPertama.firstElementChild)
 
-let content = document.getElementById("menu");
-console.log(content.firstElementChild);
+// get last child element
+let childTerakhir = document.getElementById("menu")
+console.log(childTerakhir.lastElementChild)
+
+// get all child elements
+let semuaChild = document.getElementById("menu")
+let child = menu.children
+console.log(child)
