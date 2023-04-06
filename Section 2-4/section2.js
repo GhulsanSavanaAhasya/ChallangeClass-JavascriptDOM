fcd9f85fbe92e50d76598a942f901662bd381f09
Ghulsan Savana Ahasya <ghulsan.savanaa@gmail.com>
2023-04-06 16:39:24 +0700

    memberikan komen keterangan pada contoh

--- Section 2-4/section2.js
+++ Section 2-4/section2.js
@@ -1,5 +1,8 @@
 const element = document.getElementById("intro");
 
+// ===================== Contoh 1 =========================
 element.style.color = "blue";
+// ===================== Contoh 2 =========================
 element.style.fontWeight = "bold";
+// ===================== Contoh 3 =========================
 element.style.textAlign = "center";
