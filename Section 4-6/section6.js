5d72bb9ceb04092f448e128657d4310718a519a8
Ghulsan Savana Ahasya <ghulsan.savanaa@gmail.com>
2023-04-06 23:11:39 +0700

    membuat file html dan js section6, kemudian membuat isi html dan memberikan 2 contoh pada jajavascript

---
+++ Section 4-6/section6.js
@@ -0,0 +1,9 @@
+// ===================== Contoh 1 =================
+// mengubah warna style h1 menjadi merah
+let h1 = document.querySelector("#title")
+h1.style.color = "red"
+
+// ===================== Contoh 2 =================
+// mengubah font style h1 menjadi miring
+let p = document.querySelector("#paragraph")
+p.style.fontWeight = "bold"
