be0735221d2b93ffe553eef1871ca180e0393c0e
Ghulsan Savana Ahasya <ghulsan.savanaa@gmail.com>
2023-04-06 16:31:29 +0700

    membuat 3 contoh pada section4.js

--- Section 2-4/section4.js
+++ Section 2-4/section4.js
@@ -1,2 +1,19 @@
-let minuman = document.getElementById("minuman-favorit")
-console.log(minuman.innerHTML)
+// ================ Contoh 1 =======================
+// menampilkan isi list
+let contoh1 = document.getElementById("minuman-favorit")
+console.log(contoh1.innerHTML)
+
+// ================ Contoh 2 ========================
+// menambahkan list baru
+let contoh2 = document.getElementById("minuman-favorit")
+let list = document.createElement("li")
+list.textContent = "Es jeruk"
+// masukkan kedala element ul
+contoh2.appendChild(list)
+
+console.log(contoh2.innerHTML)
+
+// ================ Contoh 3 ========================
+let contoh3 = document.getElementById("minuman-favorit")
+// menghilangkan semua list
+contoh3.innerHTML = ""
